c0bef600f5f18337448cf60539ece150f0c742fd
Janis Krasemann <janis.krasemann@enpit.de>
2020-12-06 00:16:52 +0100

    Finish day 2 pt. 1

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,3 +1,70 @@
+use std::fs;
+use regex::Regex;
+
+struct Policy {
+    min_amount: i32,
+    max_amount: i32,
+    letter: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    let puzzle = read_puzzle();
+    let mut matching_password_amount = 0;
+    for line in puzzle.lines() {
+        let policy = get_policy(line);
+        let password = get_password(line);
+        if policy.is_some() && password != "not found" {
+            if is_match(password, policy.unwrap()) {   
+                println!("Password Match! {}", password);
+                matching_password_amount += 1;
+            } else {
+                println!("Password Rejected! {}", password);
+            }
+        }
+    }
+    println!("Matching passwords total: {}", matching_password_amount);
+}
+
+fn read_puzzle() -> String {
+    println!("Reading puzzle input...");
+    fs::read_to_string("puzzle")
+        .expect("Error :(")
+}
+
+fn get_policy(line: &str) -> Option<Policy> {
+    let re = Regex::new(r"^([1-9]+[0-9]*)\-([1-9]+[0-9]*)\s([a-z])").unwrap();
+    if re.is_match(line) {
+        let cap = re.captures(line).unwrap();
+        let min_amount = cap.get(1).unwrap().as_str();
+        let max_amount = cap.get(2).unwrap().as_str();
+        let letter = cap.get(3).unwrap().as_str();
+        let policy = Policy {
+            min_amount: min_amount.parse::<i32>().unwrap(),
+            max_amount: max_amount.parse::<i32>().unwrap(),
+            letter: String::from(letter)
+        };
+        Some(policy)
+    } else {
+        None
+    }
+}
+
+fn get_password(line: &str) -> &str {
+    let re = Regex::new(r"[a-z]{2,}$").unwrap();
+    if re.is_match(line) {
+        let cap = re.captures(line).unwrap();
+        cap.get(0).unwrap().as_str()    
+    } else {
+        "not found"
+    }
+}
+
+fn is_match(password: &str, policy: Policy) -> bool {
+    let mut amount = 0;
+    for c in password.chars() {
+        if c.to_string() == policy.letter {
+            amount += 1;
+        }
+    }
+    amount >= policy.min_amount && amount <= policy.max_amount
 }
