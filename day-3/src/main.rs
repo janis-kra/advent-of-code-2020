d7545e6b194c1a7fe4eb2456c57981b0ebb1cbc6
Janis Krasemann <janis.krasemann@enpit.de>
2020-12-06 01:34:32 +0100

    Finish day 3 🤞🏻

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -2,17 +2,39 @@ use std::fs;
 
 fn main() {
     let puzzle = read_puzzle();
-    let rows = puzzle.lines();
+    let mut trees = 1;
+    let lines = puzzle.lines();
+
+    trees *= count_trees(&puzzle, 1, 1);
+    trees *= count_trees(&puzzle, 3, 1);
+    trees *= count_trees(&puzzle, 5, 1);
+    trees *= count_trees(&puzzle, 7, 1);
+    trees *= count_trees(&puzzle, 1, 2);
+    
+    println!("Trees: {}", trees);
+}
+
+fn count_trees(puzzle: &String, steps_right: i32, steps_down: i32) -> i32 {
     let mut position = 0;
     let mut trees = 0;
+    let mut rows = puzzle.lines();
+    let mut row = rows.next();
 
-    for row in rows {
-        if row.chars().nth(position % row.len()).unwrap().to_string() == "#" {
-            trees += 1;
+    while row != None {
+        if row.is_some() {
+            let line = row.unwrap();
+            if line.chars().nth(position % line.len()).unwrap().to_string() == "#" {
+                trees += 1;
+            }
         }
-        position += 3;
+        
+        position += steps_right as usize;
+
+        for i in 0..steps_down {
+            row = rows.next();
+        }
     }
-    println!("Trees: {}", trees);
+    trees
 }
 
 fn read_puzzle() -> String {
